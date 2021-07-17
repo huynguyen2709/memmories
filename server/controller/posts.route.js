9800a618e2c8e4f68d61328d10b2d83711ff684f
Huy <s3825059@rmit.edu.vn>
2021-07-17 20:04:31 +0700

    Add delete and like function

--- server/controller/posts.route.js
+++ server/controller/posts.route.js
@@ -32,4 +32,23 @@ export const updatePost = async (req, res) => {
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
     res.json(updatedPost)
-}
+};
+
+export const deletePost = async (req,res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("Cannot Delete");
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json( { message: 'Delete Succesfully' } );
+};
+
+export const likePost = async (req,res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("Cannot Like Post");
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true });
+
+    res.json(updatedPost);
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getPosts, createPosts, updatePost }from '../controller/posts.route.js';
+import { getPosts, createPosts, updatePost, deletePost, likePost }from '../controller/posts.route.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPosts);
 router.patch('/:id', updatePost);
-
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
